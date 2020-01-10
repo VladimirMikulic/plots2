a8181b808e42323d48180d2d7fb18ad6cae1f8e0
Vladimir <vladimir.mikulic2@gmail.com>
2020-01-10 19:16:19 +0100

    REFACTOR: return_to system test test
    return_to query parameter test moved from system/screenshots_test.rb
    to system/sessions_test.rb.

---
+++ test/system/sessions_test.rb
@@ -0,0 +1,14 @@
+require "application_system_test_case"
+
+class SessionsTest < ApplicationSystemTestCase
+  Capybara.default_max_wait_time = 60
+
+  test 'return_to query parameter preserved while switching from /login to /signup' do
+    visit '/login?return_to=page'
+    click_on 'sign up'
+
+    path = URI.parse(current_url).request_uri
+    assert_equal path, '/signup?return_to=page'
+  end
+  
+end
